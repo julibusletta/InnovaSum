8835f866c1422a52cc23b4b2c9fa37c7c7c1ff4b
julibusletta <julibusletta@gmail.com>
2026-09-21 23:55:31 -0300

    chore: setup Meta pixel script

--- app/components/TrackingScripts.tsx
+++ app/components/TrackingScripts.tsx
@@ -61,7 +61,17 @@ export default function TrackingScripts() {
             fbq('track', 'PageView');
           `}
         </Script>
-      )}
-    </>
-  );
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+      </>
+    )}
+  </>
+);
 }
